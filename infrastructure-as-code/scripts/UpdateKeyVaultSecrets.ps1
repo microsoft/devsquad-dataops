3f0f9b4dee27f9a748ad1ff21fbd380b6b7a5476
Marcel Aldecoa <marcel.aldecoa@microsoft.com>
2021-07-09 14:14:06 -0300

    fix KV script.
    Former-commit-id: d3534895ad429d7a0635064348ebdb29d4a78b33

--- infrastructure-as-code/scripts/UpdateKeyVaultSecrets.ps1
+++ infrastructure-as-code/scripts/UpdateKeyVaultSecrets.ps1
@@ -11,17 +11,17 @@ $keyVaultName = $DeploymentOutput["keyVaultName"]
 $ErrorActionPreference = "Stop"
 
 $context = Get-AzContext
-Write-Host "Getting user information..." -ForegroundColor Green
-$user = Get-AzADUser -UserPrincipalName $context.Account.Id
+Write-Host "Getting Service Principal information..." -ForegroundColor Green
+$servicePrincipal = Get-AzADServicePrincipal -ApplicationId $context.Account.Id
 
 Write-Host "Reading the Key Vault..." -ForegroundColor Green
 $kv = Get-AzKeyVault -VaultName $KeyVaultName
 
 Write-Host "Adding permissions to user on Key Vault..." -ForegroundColor Green
-$userPermissions = $kv.AccessPolicies | Where-Object { $_.ObjectId -eq $user.Id }
+$userPermissions = $kv.AccessPolicies | Where-Object { $_.ObjectId -eq $servicePrincipal.Id }
 $secretPermissions = $userPermissions.PermissionsToSecrets
 if (! $secretPermissions || ! $userPermissions.PermissionsToSecrets.Contains("set")) {
-    Set-AzKeyVaultAccessPolicy -VaultName $KeyVaultName -ObjectId $user.Id -PermissionsToSecrets "set"
+    Set-AzKeyVaultAccessPolicy -VaultName $KeyVaultName -ObjectId $servicePrincipal.Id -PermissionsToSecrets "set"
 }
 
 Write-Host "Add the Key Vault Secret..."
