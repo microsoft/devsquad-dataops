e4832e83365cc315bca0c1666d6b01f789b4192a
Renan Vilas Novas <vnovas.renan@gmail.com>
2021-09-22 14:53:22 -0300

    refactor: removed large files
    Former-commit-id: e8c0682f899e345bc7687820a5c706a40bf9ea33

--- infrastructure-as-code/scripts/UploadSampleData.ps1
+++ infrastructure-as-code/scripts/UploadSampleData.ps1
@@ -12,6 +12,14 @@ $ContainerName = "flights-data"
 
 $uploadStorage = Get-AzStorageAccount -ResourceGroupName $ResourceGroupName -Name $StorageAccountName
 
+mkdir ./sample-data
+$samplesStorage = 'https://stlabvm.blob.core.windows.net/sample-data/'
+$sampleFiles = ('FlightWeatherWithAirportCode.csv','FlightDelaysWithAirportCodes.csv','AirportCodeLocationLookupClean.csv')
+
+foreach ($sample in $sampleFiles){
+    Invoke-WebRequest -Uri $samplesStorage$sample -OutFile "./sample-data/$sample"
+}
+
 Set-AzStorageBlobContent -Container $ContainerName -File ./sample-data/AirportCodeLocationLookupClean.csv -Context $uploadStorage.Context -Force
 Set-AzStorageBlobContent -Container $ContainerName -File ./sample-data/FlightDelaysWithAirportCodes.csv -Context $uploadStorage.Context -Force
 Set-AzStorageBlobContent -Container $ContainerName -File ./sample-data/FlightWeatherWithAirportCode.csv -Context $uploadStorage.Context -Force
