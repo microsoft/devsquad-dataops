db310f4990ea875cf41d8c2a455979182124fbcd
fabiohaifa <61516935+fabiohaifa@users.noreply.github.com>
2022-01-18 09:56:33 -0300

    chore: add function to handle PAT

--- quickstart/scripts/modules/AzureDevOps.psm1
+++ quickstart/scripts/modules/AzureDevOps.psm1
@@ -134,19 +134,33 @@ function CloneRepo {
         $domainGitUrl = $repoInfo.remoteUrl
 
         if ($UsePAT) {
-            $domainGitUrl = $domainGitUrl -replace "(?<=https://\s*).*?(?=\s*@)", $env:AZURE_DEVOPS_EXT_PAT
+           $domainGitUrl = AddPATGitDomain -DomainGitUrl $domainGitUrl
         }
     }
-    Write-Host "Clone repo:"
-    Write-Host $domainGitUrl
-    Write-Host "Uses PAT:"
-    Write-Host $UsePAT
-    Start-Sleep -s 5
 
     git clone $domainGitUrl $directory
     
     return $directory[1]
 }
+
+function AddPATGitDomain {
+    [cmdletbinding()]
+    [OutputType([string])]
+    param (
+        [Parameter(Mandatory)] [string] $DomainGitUrl
+    )
+
+    $domainURI = [System.Uri]$DomainGitUrl
+
+    $patAdded = $env:AZURE_DEVOPS_EXT_PAT + "@" + $domainURI.Host.Split(".")[0]
+
+    $result = $domainURI.Scheme + "://" + ($domainURI.Host -Replace $domainURI.Host.Split(".")[0], $patAdded)
+
+    $result = $result + $domainURI.AbsolutePath
+
+    return $result 
+}
+
 function CreateAzDevOpsRepoApprovalPolicy {
     [cmdletbinding()]
     param (
