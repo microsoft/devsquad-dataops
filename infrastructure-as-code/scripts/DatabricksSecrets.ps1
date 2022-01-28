7cacd5e8495ec37171f85a7f1558698882bb9b77
fabiohaifa <61516935+fabiohaifa@users.noreply.github.com>
2022-01-28 15:53:00 -0300

    chore: adjustments on DatabricksSecrets process

--- infrastructure-as-code/scripts/DatabricksSecrets.ps1
+++ infrastructure-as-code/scripts/DatabricksSecrets.ps1
@@ -1,52 +1,64 @@
 param(
-    [Parameter(Mandatory)] [string] $ClientID,
-    [Parameter(Mandatory)] [securestring] $ClientSecret,
+    [Parameter(Mandatory)] [string] $ServicePrincipalName,
     [Parameter(Mandatory)] [string] $DataResourceGroup,
-    [Parameter(Mandatory)] [string] $ComputeResourceGroup,
-    [Parameter(Mandatory)] [string] $KeyVaultName,
-    [Parameter(Mandatory)] [string] $DataLakeName,
-    [Parameter(Mandatory)] [string] $DatabricksName
+    [Parameter(Mandatory)] [string] $ComputeResourceGroup
 )
 
 $ErrorActionPreference = "Stop"
 
-$context = Get-AzContext
+# Load enviornments variables from DevOps Library
+$KeyVaultName = $env:keyVaultName
+$DataLakeName = $env:dataLakeName
+$DatabricksName = $env:databricksName
 
+$context = Get-AzContext
 Write-Host "Getting user and principal information..." -ForegroundColor Green
 $user = Get-AzADUser -UserPrincipalName $context.Account.Id
-$principal = Get-AzADServicePrincipal -ApplicationId $ClientID
+$servicePrincipal = Get-AzADServicePrincipal -DisplayName $ServicePrincipalName
 
-Write-Host "Getting Azure resources..." -ForegroundColor Green
-$kv = Get-AzKeyVault -VaultName $KeyVaultName
-$lake = Get-AzStorageAccount -ResourceGroupName $DataResourceGroup -Name $DataLakeName 
-$dbw = Get-AzDatabricksWorkspace -ResourceGroupName $ComputeResourceGroup -Name $DatabricksName 
+if ($servicePrincipal) {
+        
+    Write-Host "Generating new client secret..." -ForegroundColor Green
+    $startDate = Get-Date
+    $endDate = $start.AddMonths(6)
+    $clientSecret = New-AzADSpCredential -ObjectId $servicePrincipal.Id -StartDate $startDate -EndDate $endDate
+    Write-Host "Secret generated " $clientSecret -ForegroundColor Yellow
 
-Write-Host "Adding permissions to user on Key Vault..." -ForegroundColor Green
-$userPermissions = $kv.AccessPolicies | Where-Object { $_.ObjectId -eq $user.Id }
-$secretPermissions = $userPermissions.PermissionsToSecrets
-if (! $secretPermissions || ! $userPermissions.PermissionsToSecrets.Contains("set")) {
-    Set-AzKeyVaultAccessPolicy -VaultName $KeyVaultName -ObjectId $user.Id -PermissionsToSecrets "set"
+    Write-Host "Getting Azure resources..." -ForegroundColor Green
+    $kv = Get-AzKeyVault -VaultName $KeyVaultName
+    $lake = Get-AzStorageAccount -ResourceGroupName $DataResourceGroup -Name $DataLakeName 
+    $dbw = Get-AzDatabricksWorkspace -ResourceGroupName $ComputeResourceGroup -Name $DatabricksName 
+
+    Write-Host "Adding permissions to user on Key Vault..." -ForegroundColor Green
+    $userPermissions = $kv.AccessPolicies | Where-Object { $_.ObjectId -eq $user.Id }
+    $secretPermissions = $userPermissions.PermissionsToSecrets
+    if (! $secretPermissions || ! $userPermissions.PermissionsToSecrets.Contains("set")) {
+        Set-AzKeyVaultAccessPolicy -VaultName $KeyVaultName -ObjectId $user.Id -PermissionsToSecrets "set"
+    }
+
+    Write-Host "Setting service principal secrets on Key Vault..." -ForegroundColor Green
+    Set-AzKeyVaultSecret -VaultName $KeyVaultName -Name "tenantId" -SecretValue $(ConvertTo-SecureString $context.Tenant.Id -AsPlainText -Force)
+    Set-AzKeyVaultSecret -VaultName $KeyVaultName -Name "clientId" -SecretValue $(ConvertTo-SecureString $ClientID -AsPlainText -Force)
+    Set-AzKeyVaultSecret -VaultName $KeyVaultName -Name "clientSecret" -SecretValue $clientSecret
+
+    Write-Host "Assigning roles to the service principal on the data lake..." -ForegroundColor Green
+    $assigment = Get-AzRoleAssignment -ObjectId $principal.Id -Scope $lake.Id | Where-Object { $_.RoleDefinitionName -eq "Storage Blob Data Contributor" }
+    if(! $assigment){
+        New-AzRoleAssignment -ObjectId $principal.Id -Scope $lake.Id -RoleDefinitionName "Storage Blob Data Contributor" 
+    }
+
+    Write-Host "Creating the Key Vault secret scope on Databricks..." -ForegroundColor Green
+    $accessToken = Get-AzAccessToken -ResourceUrl 2ff814a6-3304-4ab8-85cb-cd0e6f879c1d
+    $env:DATABRICKS_TOKEN = $accessToken.Token
+    $env:DATABRICKS_HOST = "https://$($dbw.Url)"
+    Write-Host "URL DBW https://$($dbw.Url)"
+    $scopesList = databricks secrets list-scopes --output json | ConvertFrom-Json
+    if (! $scopesList.scopes.name -contains "dataops") {
+        databricks secrets create-scope --scope 'dataops' --scope-backend-type AZURE_KEYVAULT --resource-id $kv.ResourceId --dns-name $kv.VaultUri
+    }
 }
-
-Write-Host "Setting service principal secrets on Key Vault..." -ForegroundColor Green
-Set-AzKeyVaultSecret -VaultName $KeyVaultName -Name "tenantId" -SecretValue $(ConvertTo-SecureString $context.Tenant.Id -AsPlainText -Force)
-Set-AzKeyVaultSecret -VaultName $KeyVaultName -Name "clientId" -SecretValue $(ConvertTo-SecureString $ClientID -AsPlainText -Force)
-Set-AzKeyVaultSecret -VaultName $KeyVaultName -Name "clientSecret" -SecretValue $ClientSecret
-
-Write-Host "Assigning roles to the service principal on the data lake..." -ForegroundColor Green
-$assigment = Get-AzRoleAssignment -ObjectId $principal.Id -Scope $lake.Id | Where-Object { $_.RoleDefinitionName -eq "Storage Blob Data Contributor" }
-if(! $assigment){
-    New-AzRoleAssignment -ObjectId $principal.Id -Scope $lake.Id -RoleDefinitionName "Storage Blob Data Contributor" 
-}
-
-Write-Host "Creating the Key Vault secret scope on Databricks..." -ForegroundColor Green
-$accessToken = Get-AzAccessToken -ResourceUrl 2ff814a6-3304-4ab8-85cb-cd0e6f879c1d
-$env:DATABRICKS_TOKEN = $accessToken.Token
-$env:DATABRICKS_HOST = "https://$($dbw.Url)"
-Write-Host "URL DBW https://$($dbw.Url)"
-$scopesList = databricks secrets list-scopes --output json | ConvertFrom-Json
-if (! $scopesList.scopes.name -contains "dataops") {
-    databricks secrets create-scope --scope 'dataops' --scope-backend-type AZURE_KEYVAULT --resource-id $kv.ResourceId --dns-name $kv.VaultUri
+else {
+    Write-Host "No Service Principal founded" -ForegroundColor Red
 }
 
 Write-Host "Finished!" -ForegroundColor Blue
