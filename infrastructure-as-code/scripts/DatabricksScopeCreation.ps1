fe47730da5078d7e537223aff214f169e7048374
fabiohaifa <61516935+fabiohaifa@users.noreply.github.com>
2022-02-10 14:41:28 -0300

    chore: script adjustment

--- infrastructure-as-code/scripts/DatabricksScopeCreation.ps1
+++ infrastructure-as-code/scripts/DatabricksScopeCreation.ps1
@@ -14,8 +14,9 @@ $dbw = Get-AzDatabricksWorkspace -ResourceGroupName $ComputeResourceGroup -Name 
 Write-Host "Creating the Key Vault secret scope on Databricks..." -ForegroundColor Green
 $accessToken = Get-AzAccessToken -ResourceUrl 2ff814a6-3304-4ab8-85cb-cd0e6f879c1d
 $env:DATABRICKS_TOKEN = $accessToken.Token
-#$env:DATABRICKS_HOST = "https://$($dbw.Url)"
-#Write-Host "URL DBW https://$($dbw.Url)"
+$env:DATABRICKS_HOST = "https://$($dbw.Url)"
+Write-Host "URL DBW https://$($dbw.Url)"
+
 $scopesList = databricks secrets list-scopes --output json | ConvertFrom-Json
 if (! $scopesList.scopes.name -contains "dataops") {
     databricks secrets create-scope --scope 'dataops' --scope-backend-type AZURE_KEYVAULT --resource-id $kv.ResourceId --dns-name $kv.VaultUri
